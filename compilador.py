3b98ecf490af94ce94f029292f108676dfc074c4
Joao Duarte Laranjo Cerquido <62067073+Grodd074@users.noreply.github.com>
2022-05-15 23:31:26 +0100

    fixed literals

--- compilador.py
+++ compilador.py
@@ -6,11 +6,9 @@ import re
 import math
 
 # ANALISADOR LÉXICO
+literals = ['(', ')', '[', ']', '{', '}', '"', '\'', ',', '.', ':', '\\', '=', '%', '+', '-', '*', '/']
 reservadas = "tokens literals ignore return error yacc lex precedence ts def".split()
-tokens = ['EQUALS', 'PERC', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-          'LBRAC', 'RBRAC', 'LRBRAC', 'RRBRAC', 'LCHAV', 'RCHAV',
-          'QUOTE', 'PELICA', 'COMMA', 'DOT', 'DDOT', 'BACKSLASH',
-          'SSTR', 'STR', 'REGEX', 'NUMBER', 'INDEX', 'LITERAL',
+tokens = ['SSTR', 'STR', 'REGEX', 'NUMBER', 'INDEX', 'LITERAL',
           'LIST', 'CHAVSTXT', 'BEGINCODE', 'ALL'] + [x.upper() for x in reservadas]
 states = (('incode', 'exclusive'),)
 
@@ -59,24 +57,6 @@ def t_BEGINCODE(t):
     t.lexer.begin('incode')
     return t
 
-def t_LBRAC(t):      r'\('; return t
-def t_RBRAC(t):      r'\)'; return t
-def t_LRBRAC(t):     r'\['; return t
-def t_RRBRAC(t):     r'\]'; return t
-def t_LCHAV(t):      r'\{'; return t
-def t_RCHAV(t):      r'\}'; return t
-def t_QUOTE(t):      r'\"'; return t
-def t_PELICA(t):     r'\''; return t
-def t_COMMA(t):      r',' ; return t
-def t_DOT(t):        r'\.'; return t
-def t_DDOT(t):       r':' ; return t
-def t_BACKSLASH(t):  r'\\'; return t
-def t_EQUALS(t):     r'=' ; return t
-def t_PERC(t):       r'%' ; return t
-def t_PLUS(t):       r'\+'; return t
-def t_MINUS(t):      r'-' ; return t
-def t_TIMES(t):      r'\*'; return t
-def t_DIVIDE(t):     r'/' ; return t
 
 t_ignore = " \t\n"
 t_incode_ignore = "\n"
@@ -113,11 +93,11 @@ lexer = lex()
 """
 
 def p_IGN(p):
-    "IGN : PERC IGNORE EQUALS SSTR"
+    "IGN : '%' IGNORE '=' SSTR"
     p[0] = f'ignore = {p[4]}'
 
 def p_LIT(p): 
-    "LIT : PERC LITERALS EQUALS SSTR"
+    "LIT : '%' LITERALS '=' SSTR"
     p[0] = f'literals = {p[4]}'
     p[4] = p[4][:-1]
     p[4] = p[4][1:]
@@ -125,7 +105,7 @@ def p_LIT(p):
         ts['ltls'].append(x)
 
 def p_TOK(p):
-    "TOK : PERC TOKENS EQUALS LIST"
+    "TOK : '%' TOKENS '=' LIST"
     p[0] = f'tokens = {p[4]}'
 
 
@@ -133,7 +113,7 @@ def p_TRULES_1(p): "TRULES : TRULE TRULES" ; p[0] = p[1] + p[2]
 def p_TRULES_2(p): "TRULES : TERR"         ; p[0] = p[1]
 
 def p_TRULE_1(p):
-    "TRULE : REGEX RETURN LBRAC SSTR COMMA ARGS RBRAC"
+    "TRULE : REGEX RETURN '(' SSTR ',' ARGS ')'"
     p[4] = p[4][1:]
     p[4] = p[4][:-1]
     p[0] = f"""def t_{p[4]}(t):
@@ -143,27 +123,27 @@ def p_TRULE_1(p):
 """
 
 def p_TERR_1(p):
-    "TERR : REGEX ERROR LBRAC INSTS RBRAC"
+    "TERR : REGEX ERROR '(' INSTS ')'"
     p[0] = f"""def t_error(t):
     {p[1]}
     {p[4]}
 """
 
 def p_INSTS_1(p):
-    "INSTS : ARGS COMMA ARGS"
+    "INSTS : ARGS ',' ARGS"
     p[0] = f"""{p[1]}
     {p[3]}
 """
 
-def p_ARGS_1(p):  "ARGS : ARGS COMMA ARGS"          ;  p[0] = f"{p[1]}, {p[3]}"
+def p_ARGS_1(p):  "ARGS : ARGS ',' ARGS"          ;  p[0] = f"{p[1]}, {p[3]}"
 def p_ARGS_2(p):  "ARGS : ARGS ARGS"                ;  p[0] = f"{p[1]} {p[3]}"
-def p_ARGS_3(p):  "ARGS : ARGS DOT ARGS"            ;  p[0] = f"{p[1]}.{p[3]}"
-def p_ARGS_4(p):  "ARGS : ARGS LBRAC ARGS RBRAC"    ;  p[0] = f"{p[1]}({p[3]})"
-def p_ARGS_5(p):  "ARGS : ARGS EQUALS ARGS"         ;  p[0] = f"{p[1]} = {p[3]}"
-def p_ARGS_6(p):  "ARGS : ARGS PLUS ARGS"           ;  p[0] = f"{p[1]} + {p[3]}"
-def p_ARGS_7(p):  "ARGS : ARGS MINUS ARGS"          ;  p[0] = f"{p[1]} - {p[3]}"
-def p_ARGS_8(p):  "ARGS : ARGS TIMES ARGS"          ;  p[0] = f"{p[1]} * {p[3]}"
-def p_ARGS_9(p):  "ARGS : ARGS DIVIDE ARGS"         ;  p[0] = f"{p[1]} / {p[3]}"
+def p_ARGS_3(p):  "ARGS : ARGS '.' ARGS"            ;  p[0] = f"{p[1]}.{p[3]}"
+def p_ARGS_4(p):  "ARGS : ARGS '(' ARGS ')'"    ;  p[0] = f"{p[1]}({p[3]})"
+def p_ARGS_5(p):  "ARGS : ARGS '=' ARGS"         ;  p[0] = f"{p[1]} = {p[3]}"
+def p_ARGS_6(p):  "ARGS : ARGS '+' ARGS"           ;  p[0] = f"{p[1]} + {p[3]}"
+def p_ARGS_7(p):  "ARGS : ARGS '-' ARGS"          ;  p[0] = f"{p[1]} - {p[3]}"
+def p_ARGS_8(p):  "ARGS : ARGS '*' ARGS"          ;  p[0] = f"{p[1]} * {p[3]}"
+def p_ARGS_9(p):  "ARGS : ARGS '/' ARGS"         ;  p[0] = f"{p[1]} / {p[3]}"
 def p_ARGS_10(p): "ARGS : ARG"                      ;  p[0] = p[1]
 
 def p_ARG_1(p):  "ARG : STR"                     ;   p[0] = p[1]
@@ -181,18 +161,18 @@ def p_GRAM_1(p):
 """
 
 def p_TSYM_1(p):
-    "TSYM : TS EQUALS CHAVSTXT"
+    "TSYM : TS '=' CHAVSTXT"
     p[0] = f"ts = {p[3]}\n"
 
 def p_PRCDNC_1(p):
-    "PRCDNC : PERC PRECEDENCE EQUALS LIST"
+    "PRCDNC : '%' PRECEDENCE '=' LIST"
     p[0] = f'precedence = {p[4]}\n'
 
 def p_GRULES_1(p):  "GRULES : GRULES GRULE"  ;  p[0] = p[1] + p[2]
 def p_GRULES_2(p):  "GRULES : GRULE"         ;  p[0] = p[1]
 
 def p_GRULE_1(p):
-    "GRULE : STR DDOT PARAMS CHAVSTXT"
+    "GRULE : STR ':' PARAMS CHAVSTXT"
     p[4] = p[4][2:]
     p[4] = p[4][:-2]
     if p[1] in ts:
@@ -228,7 +208,7 @@ def p_PARAM_2(p):
 
 
 def p_PARAM_3(p):
-    "PARAM : PERC STR"
+    "PARAM : '%' STR"
     p[0] = p[1] + p[2]
 
 def p_error(p):
